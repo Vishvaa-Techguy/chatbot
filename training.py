2b7da11e4aafeecbfc937c27150cdc277abeb0ad
Vishvaa-Techguy <kvishvaa6@gmail.com>
2022-04-06 22:06:12 +0530

    main

--- training.py
+++ training.py
@@ -49,7 +49,7 @@ training = np.array(training)
 
 train_x = list(training[:,0])
 train_y = list(training[:,1])
-
+# Neural Network
 model = Sequential()
 model.add(Dense(128,input_shape=(len(train_x[0]),),activation='relu'))
 model.add(Dropout(0.5))
@@ -58,7 +58,7 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]),activation = "softmax"))
 
 sgd = SGD(lr=0.01,decay = 1e-6,momentum=0.9,nesterov=True)
-model.compile(loss = "categorical_crossentropy" , optimizer = sgd , metrics=['accuracy'])
+model.compile(loss = "categorical_crossentropy", optimizer = sgd , metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x),np.array(train_y),epochs=200 , batch_size = 5, verbose=1)
 model.save('chatbotmodel.h5',hist)
